2e45981ab7ec44c72bd5ed1f11d7889546d12285
Tom van Diggelen <49716596+tomvandig@users.noreply.github.com>
2024-06-21 08:53:13 +0200

    Build hierarchy

--- hello_wall.ts
+++ hello_wall.ts
@@ -5,20 +5,41 @@ import { ECSID, Rel } from "./sm_primitives";
 
 let ecs = new ECS();
 
+// define a typical
+let typical_wall = new ECSID(["typical_wall"]);
+{
+    let left_window = new ECSID(["left_window"]);
+    ecs.AddComponent(left_window, "classification", new ifc_window());
+    let right_window = new ECSID(["right_window"]);
+    ecs.AddComponent(right_window, "classification", new ifc_window());
+    let window_frame = new ECSID(["window_frame"]);
+    ecs.AddComponent(window_frame, "classification", new ifc_windowframe());
 
+    ecs.AddParent(left_window, typical_wall);
+    ecs.AddParent(right_window, typical_wall);
+
+    ecs.AddParent(window_frame, right_window);
+    ecs.AddParent(window_frame, left_window);
+}
+
+// define all elements
 let south_wall = new ECSID(["south_wall"]);
 ecs.AddComponent(south_wall, "classification", new ifc_wall());
-
-let typical_wall = new ECSID(["typical_wall"]);
-let left_window = new ECSID(["left_window"]);
-ecs.AddComponent(left_window, "classification", new ifc_window());
-let right_window = new ECSID(["right_window"]);
-ecs.AddComponent(right_window, "classification", new ifc_window());
-let window_frame = new ECSID(["window_frame"]);
-ecs.AddComponent(window_frame, "classification", new ifc_windowframe());
+let north_wall = new ECSID(["north_wall"]);
+ecs.AddComponent(north_wall, "classification", new ifc_wall());
 let space = new ECSID(["space"]);
 ecs.AddComponent(space, "classification", new ifc_space());
+{
+    ecs.AddParent(typical_wall, south_wall);
+    ecs.AddParent(typical_wall, north_wall);
 
+    // roots
+    ecs.AddParent(south_wall, new ECSID([]));
+    ecs.AddParent(north_wall, new ECSID([]));
+    ecs.AddParent(space, new ECSID([]));
+}
+
+// construct relationships
 {
     let spaceboundary = new ECSID(["spaceboundary"]);
     let boundaryComponent = new ifc_spaceboundary();
